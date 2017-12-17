e2b587a2dc556c10dc2e408fd514170606d94887
cnsorensen <cnsorensen0@gmail.com>
2017-12-17 15:18:28 -0700

    Added checks for user input from replies. Fixed README

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Twilight in Massachusetts - A Christine Sorney Mystery
 *Note: Game is not completed yet*
 
-This is definitely *not* a spite game against against a Female Software company and their recent lack of producing mystery games staring a Fancy detective.
+This is definitely *not* a spite game against against a Female Software company 
+and their recent lack of producing mystery games starring a Fancy detective.
 
 ## Description
-18-year-old Christine Sorney drops out of high school and travels to Salem, Massachusetts leaving her hometown of River Depths behind. But when her roommate goes missing, Christine trys to find out what happened and get wrapped in a neo-witch cult.
+18-year-old Christine Sorney drops out of high school and travels to Salem, 
+Massachusetts leaving her hometown of River Depths behind. But when her 
+roommate goes missing, Christine trys to find out what happened and gets 
+wrapped in a neo-witch cult.
--- plot.cpp
+++ plot.cpp
@@ -49,6 +49,7 @@ void Plot1()
 {
     char ret1[3];
     char ret2[3];
+    int ret_valid;
     char* err;
 
     // if the plot is being repeated
@@ -64,77 +65,105 @@ void Plot1()
     
     ENTER();
     CLEARSCREEN();
- 
-    SarahG.Speak("Hey, you must be Christine! I'm your new roommate Sarah.\n", 1, 0, 0);
 
-    // reply1 
-    ChristineS.Speak("Hi Sarah. What are you reading?", 0, 0, 1);
 
-    // reply2
-    ChristineS.Speak("I'm guessing this is my bed by the wall?", 0, 0, 2);
-
-    fprintf(stdout, "\n%sChoose one: ", TC_NORM);   
-    err = fgets(ret1, 3, stdin);
-    CLEARSCREEN();
-    
-    if(ret1[0] == '2')
+    do
     {
-        SarahG.Speak("Oh right, I hope you don't mind I picked the one by the window.", 0, 0, 0);
-        SARAHG_SAT--; 
-        ENTER();
-        CLEARSCREEN();
-    }
-    else
-    {
-        SARAHG_SAT++;
-    }
-
-    SarahG.Speak("I've been busy studying the Salem Witch Trials for my history class.", 0, 0, 0);
-    
-    // reply1
-    ChristineS.Speak("Shouldn't you know all about it already? I mean, you live here, don't you?", 0, 0, 1);
-
-    // reply2
-    ChristineS.Speak("I really don't know much about the trials.", 0, 0, 2);
-
-    fprintf(stdout, "\n%sChoose one: ", TC_NORM);   
-    err = fgets(ret1, 3, stdin);
-    CLEARSCREEN();
-
-    if(ret1[0] == '2')
-    {
-        SarahG.Speak("I can teach you, that is if you're interested.", 0, 0, 0);
+        ret_valid = -1;
+        SarahG.Speak("Hey, you must be Christine! I'm your new roommate Sarah.\n", 1, 0, 0);
 
         // reply1
-        ChristineS.Speak("Umm, I'm good. Thanks though.", 0, 0, 1);
+        ChristineS.Speak("Hi Sarah. What are you reading?", 0, 0, 1);
 
         // reply2
-        ChristineS.Speak("Sure, maybe after my first shift at the Witches Brew?", 0, 0, 2);
+        ChristineS.Speak("I'm guessing this is my bed by the wall?", 0, 0, 2);
 
         fprintf(stdout, "\n%sChoose one: ", TC_NORM);   
-        err = fgets(ret2, 3, stdin);
+        err = fgets(ret1, 3, stdin);
         CLEARSCREEN();
-
-        if(ret2[0] == '2')
+    
+        if(ret1[0] == '2')
+        {
+            SarahG.Speak("Oh right, I hope you don't mind I picked the one by the window.", 0, 0, 0);
+            SARAHG_SAT--; 
+            ENTER();
+            CLEARSCREEN();
+            ret_valid = 0;
+        }
+        else if(ret1[0] == '1')
         {
             SARAHG_SAT++;
+            ret_valid = 0;
         }
-    }
 
-    if(ret1[0] == '1' || ret2[0] == '1')
+    }while(ret_valid == -1);
+
+    do
     {
-        SarahG.Speak("Look, if you're going to not take this seriously, then don't say anything at all.", 0, 0, 0);
-        SARAHG_SAT -= 2;
+        ret_valid = -1;
+        SarahG.Speak("I've been busy studying the Salem Witch Trials for my history class.", 0, 0, 0);
+    
+        // reply1
+        ChristineS.Speak("Shouldn't you know all about it already? I mean, you live here, don't you?", 0, 0, 1);
 
-        ENTER();
+        // reply2
+        ChristineS.Speak("I really don't know much about the trials.", 0, 0, 2);
+
+        fprintf(stdout, "\n%sChoose one: ", TC_NORM);   
+        err = fgets(ret1, 3, stdin);
         CLEARSCREEN();
-    }
+
+        if(ret1[0] == '2')
+        {
+            do
+            {
+                SarahG.Speak("I can teach you, that is if you're interested.", 0, 0, 0);
+
+                // reply1
+                ChristineS.Speak("Umm, I'm good. Thanks though.", 0, 0, 1);
+
+                // reply2
+                ChristineS.Speak("Sure, maybe after my first shift at the Witches Brew?", 0, 0, 2);
+
+                fprintf(stdout, "\n%sChoose one: ", TC_NORM);   
+                err = fgets(ret2, 3, stdin);
+                CLEARSCREEN();
+
+                if(ret2[0] == '1')
+                {
+                    ret_valid = 0;
+                }
+                else if(ret2[0] == '2')
+                {
+                    SARAHG_SAT++;
+                    ret_valid = 0;
+                }
+            }while(ret_valid == -1);
+
+            ret_valid = 0;
+        }
+
+        if(ret1[0] == '1' || ret2[0] == '1')
+        {
+            SarahG.Speak("Look, if you're going to not take this seriously, then don't say anything at all.", 0, 0, 0);
+            SARAHG_SAT -= 2;
+
+            ENTER();
+            CLEARSCREEN();
+
+            ret_valid = 0;
+        }
+
+    }while(ret_valid == -1);
 
     SarahG.Speak("It's time for your first shift anyway. Let's go.", 0, 0, 0); 
-   
+
+    ENTER();
+    CLEARSCREEN(); 
     // debugging
-    printf("%d\n", SARAHG_SAT);
+    //printf("%d\n", SARAHG_SAT);
  
+    // mark this plot complete
     PLOT_FLAGS[1] = 1;
 
     return;
@@ -145,6 +174,7 @@ void Plot2()
     char* err;
     char ret1[3];
     char ret2[3];    
+    int ret_valid;
 
     // if the plot is being repeated 
     if(PLOT_FLAGS[2] == 1)
@@ -158,6 +188,7 @@ void Plot2()
     ENTER();
     CLEARSCREEN();
 
+    // mark this plot complete
     PLOT_FLAGS[2] = 1;
 
     //SaveGame();
@@ -182,6 +213,7 @@ void Plot3()
     ENTER();
     CLEARSCREEN();
 
+    // mark this plot complete
     PLOT_FLAGS[3] = 1;
 
     return;
@@ -204,6 +236,7 @@ void Plot4()
     ENTER();
     CLEARSCREEN();
 
+    // mark this plot complete
     PLOT_FLAGS[4] = 1;
 
     return;
