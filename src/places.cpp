cd521895e85e772cebeeb9344e80e13eeda5d4c3
cnsorensen <cnsorensen0@gmail.com>
2020-03-08 12:39:49 -0400

    Changed repo to Oregon and updated code to reflect that

--- src/places.cpp
+++ src/places.cpp
@@ -3,24 +3,24 @@
 #include "places.h"
 
 Place Downtown("Downtown",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/downtown-day.bmp",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/downtown-night.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/downtown-day.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/downtown-night.bmp",
 NULL, NULL, idDowntown, std::vector<Hotspot>());
 Place WitchesBrew("Witches Brew",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/witchsbrew-day.bmp",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/witchsbrew-night.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/witchsbrew-day.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/witchsbrew-night.bmp",
 NULL, NULL, idWitchesBrew, std::vector<Hotspot>());
 Place ApartmentFull("Christine and Sarah's apartment",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/roomfull-day.bmp",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/roomfull-night.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/roomfull-day.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/roomfull-night.bmp",
 NULL, NULL, idApartmentFull, std::vector<Hotspot>());
 Place ApartmentSarah("Sarah's bed",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/roomsarah-day.bmp",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/roomsarah-night.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/roomsarah-day.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/roomsarah-night.bmp",
 NULL, NULL, idApartmentSarah, std::vector<Hotspot>());
 Place SalemU("Salem University",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/salemu-day.bmp",
-"/home/cnsorensen/projects/twilight-in-massachusetts/src/images/backgrounds/salemu-night.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/salemu-day.bmp",
+"/home/cnsorensen/projects/twilight-in-oregon/src/images/backgrounds/salemu-night.bmp",
 NULL, NULL, idSalemU, std::vector<Hotspot>());
 
 int PlacesCount = 5;
